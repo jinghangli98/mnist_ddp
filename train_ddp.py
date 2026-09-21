9f341706d50020ec7e48d6f6a5381f50cf082ce1
jinghangli98 <jinghang.li@pitt.edu>
2026-09-20 21:06:00 -0400

    working ddp training

--- train_ddp.py
+++ train_ddp.py
@@ -5,6 +5,20 @@ import torch.nn.functional as F
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import CosineAnnealingLR, LinearLR, SequentialLR
 
+import torch.multiprocessing as mp
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
+import torch.distributed as dist
+import os
+
+
+def ddp_setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '10086'
+    torch.cuda.set_device(rank)
+    init_process_group(backend='nccl', rank=rank, world_size=world_size)
+    
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -51,15 +65,18 @@ def getloader(args):
     train_dataset = datasets.MNIST(args.datadir, train=True, download=True, transform=transform)
     test_dataset = datasets.MNIST(args.datadir, train=False, transform=transform)
     
-    train_loader = torch.utils.data.DataLoader(train_dataset, shuffle=True, batch_size=args.batch_size, num_workers=args.num_workers)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, num_workers=args.num_workers)
+    train_sampler = DistributedSampler(train_dataset)
+    test_sampler = DistributedSampler(test_dataset, shuffle=False)
     
-    return train_loader, test_loader
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, num_workers=args.num_workers, sampler=train_sampler)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, num_workers=args.num_workers, sampler=test_sampler)
+    
+    return train_loader, test_loader, train_sampler, test_sampler
 
-def train(model, train_loader, optimizer, epoch, device, scheduler, log_interval=100):
+def train(model, train_loader, optimizer, epoch, rank, scheduler, log_interval=100):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+        data, target = data.to(rank), target.to(rank)
         optimizer.zero_grad()
         output = model(data)
         loss = torch.nn.functional.nll_loss(output, target)
@@ -67,29 +84,37 @@ def train(model, train_loader, optimizer, epoch, device, scheduler, log_interval
         optimizer.step()
         scheduler.step()
         
-        if batch_idx % log_interval == 0:
+        if rank == 0 and batch_idx % log_interval == 0:
             print(f'Epoch: {epoch} [{batch_idx}/{len(train_loader)}]'
                   f'loss {loss.item():.4f} lr {scheduler.get_last_lr()[0]:.2e}')
     
-def test(model, test_loader, epoch, device):
+def test(model, test_loader, epoch, rank):
     model.eval()
-    accuracy = 0
+    
+    correct = torch.zeros(1, device=rank)
+    total = torch.zeros(1, device=rank)
     for batch_idx, (data, target) in enumerate(test_loader):
-        data, target = data.to(device), target.to(device)
+        data, target = data.to(rank), target.to(rank)
         with torch.no_grad():
             output = model(data)
             predictions = torch.argmax(torch.nn.Softmax(dim=1)(output), dim=1)
-            predictions == target
-            running_accuracy = torch.sum(predictions == target) / len(predictions)
-            running_accuracy = running_accuracy.item()
-            accuracy += running_accuracy
-        
-    print(f'epoch: {epoch} | running accuracy: {accuracy/(batch_idx+1):.2f}')
+            correct += (predictions == target).sum()
+            total += target.size(0)
+            
+    dist.all_reduce(total, op=dist.ReduceOp.SUM)
+    dist.all_reduce(correct, op=dist.ReduceOp.SUM)
+    accuracy = (correct/total).item()
+    
+    if rank == 0:
+        print(f'epoch: {epoch} | accuracy: {accuracy:.2f}')
          
-def main(args):
-    train_loader, test_loader = getloader(args)
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = Net().to(device)
+def main(rank, world_size, args):
+    ddp_setup(rank, world_size)
+    
+    train_loader, test_loader, train_sampler, test_sampler = getloader(args)
+    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = Net().to(rank)
+    model = DDP(model, device_ids=[rank])
     optimizer = torch.optim.Adam(model.parameters(), lr = args.lr)
     
     warmup_steps = int(0.05 * args.epochs * len(train_loader))
@@ -111,13 +136,25 @@ def main(args):
         schedulers=[warmup, cosine],
         milestones=[warmup_steps]
     )
-    print('Training starts here.......')
+    
+    if rank == 0 :
+        print('Training starts here.......')
     for epoch in range(args.epochs):
-        train(model, train_loader, optimizer, epoch, device, scheduler)
+        train_sampler.set_epoch(epoch)
+        train(model, train_loader, optimizer, epoch, rank, scheduler)
         if epoch % args.eval_freq == 0:
-            test(model, test_loader, epoch, device)
-        
+            test(model, test_loader, epoch, rank)
+    
+    if rank == 0:
+        torch.save(model.module.state_dict(), 'model.pt')
+    
+    destroy_process_group()
+    
+    
+     
 
 if __name__ == '__main__':
     args = parse_args()
-    main(args)
+    world_size = torch.cuda.device_count()
+    mp.spawn(main, args=(world_size, args), nprocs=world_size)
+    # main(args)
